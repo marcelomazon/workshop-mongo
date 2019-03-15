5bb5fcd9d6c6ab2fd626940b135d93e7d6c5c380
Marcelo Mazon <marcelo.mazon@betha.com.br>
2019-03-15 16:02:33 -0300

    ordenação por nome do usuário

--- src/main/java/com/mazon/mongo/services/UserService.java
+++ src/main/java/com/mazon/mongo/services/UserService.java
@@ -3,6 +3,7 @@ package com.mazon.mongo.services;
 import com.mazon.mongo.domain.User;
 import com.mazon.mongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class UserService {
     private UserRepository repository; // mecanismo de injeção automática do spring
 
     public List<User> findAll(){
-        List<User> lista = repository.findAll();
+        List<User> lista = repository.findAll(Sort.by("name"));
         return lista;
     }
 }
