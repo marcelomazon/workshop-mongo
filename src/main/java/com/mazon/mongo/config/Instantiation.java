ddabc9bfb1e6f477ec72af731d8d55c9a4d0abf5
Marcelo Mazon <marcelo.mazon@betha.com.br>
2019-03-19 11:49:42 -0300

    criando class Post com User aninhado

--- src/main/java/com/mazon/mongo/config/Instantiation.java
+++ src/main/java/com/mazon/mongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.mazon.mongo.config;
 
+import com.mazon.mongo.domain.Post;
 import com.mazon.mongo.domain.User;
+import com.mazon.mongo.repository.PostRepository;
 import com.mazon.mongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,17 +18,26 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll(); // deleta toda coleção
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        userRepository.save(maria);
-        userRepository.save(alex);
-        userRepository.save(bob);
+        Post post1 = new Post(null, sdf.parse("18/03/2019"), "Partiu viagem","Vou viajar para SP - Abraço!", maria);
+        Post post2 = new Post(null, sdf.parse("19/03/2019"), "Bom dia", "Esse spring é do carvalho", maria);
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/mazon/mongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.mazon.mongo.repository;
+
+import com.mazon.mongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
